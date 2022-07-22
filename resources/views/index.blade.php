b47d0b671759aab77efe8995f10cb8eaf72aa82d
Cristobalam <crisaraos@hotmail.com>
2022-07-21 20:43:05 -0400

    carga final

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container mt-3 h1" align="center">
+<div class="container text-center">
     <h1>Bienvenido</h1>
     <h4>Enterate de todos los productos disponibles que tenemos en stock</h4>
     <img src="https://www.republicainmobiliaria.com/wp-content/uploads/2019/05/Blog-silca.jpg" class="rounded img-fluid">
